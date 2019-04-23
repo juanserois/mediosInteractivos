f0f4babb52baf2f223c2abe87eee047edd3f0cc2
Juan Rojas <42258025+juanserois@users.noreply.github.com>
2019-04-23 14:04:28 -0500

    Update sketch.js

--- 10/sketch.js
+++ 10/sketch.js
@@ -1,4 +1,6 @@
-var tablaEducation
+var tablaEducation;
+
+var img;
 
 var numfilas;
 var numColumnas;
@@ -7,20 +9,21 @@ var ratioPrev = 0;
 
 function preload() {
   tablaEducation = loadTable('assets/education.csv', 'csv', 'header');
+  img = loadImage('assets/colombia.jpg')
 }
 
 function setup() {
-  createCanvas(windowWidth, 1400);
-  background(0);
+  createCanvas(1200, 1350);
+  image(img, 0, 0, 1200, 1350);
+  background(0, 0, 0, 200);
 
   numFilas = tablaEducation.getRowCount();
   numColumnas = tablaEducation.getColumnCount();
 
 
   for (var i = 0; i < numFilas; i++) {
-    color = (map (contador, 0, 10 , 0 ,255))
-    color = (map (color, 0, 500 , 0 ,255))
-    print (color)
+    color = (map(contador, 0, 10, 0, 255))
+    color = (map(color, 0, 500, 0, 255))
     var pais = tablaEducation.getString(i, 1);
     if (pais == "Colombia") {
       var ano = tablaEducation.getNum(i, 2);
@@ -29,7 +32,7 @@ function setup() {
       var splitString = split(valor, ',');
       valor = float(splitString[0] + splitString[1]);
 
-      fill(255-color/2, color, color/2);
+      fill(255 - color / 2, color, color / 2);
       if (contador % 3 == 1) {
         text(ano, 10, 30 * contador);
         text(serie, 50, 30 * contador);
@@ -37,9 +40,9 @@ function setup() {
         rect(380, 30 * contador - 8, ancho, 20)
         fill(255);
         if (color > 200)
-        fill(0);
-        text ( valor, 385 ,30 * contador +7)
-        
+          fill(0);
+        text(valor, 385, 30 * contador + 7)
+
       } else {
         splitString = split(serie, '(');
         if (splitString[1] == "male)") {
@@ -48,8 +51,8 @@ function setup() {
         if (splitString[1] == "female)") {
           text("Female to male percentage", 10, 30 * contador - 30);
           var percentage = (valor / ratioPrev) * 100;
-          text(percentage + " %", 380, 30 * contador- 30);
-         
+          text(percentage + " %", 380, 30 * contador - 30);
+
         }
       }
       contador++;
